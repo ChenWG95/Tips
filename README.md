e01b8f09b9459bcfeee1a3d702bf5f11272a5075
Quaner <1227646458@qq.com>
2021-04-06 12:43:02 +0800

    chore: 生成目录脚本

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Tips
+
+- [1-Chrome特殊URL.md](./Chrome特殊URL.md)
+- [2-关闭Chrome安全搜索.md](./关闭Chrome安全搜索.md)
---
+++ build-list.js
@@ -0,0 +1,27 @@
+const fs = require('fs')
+
+
+async function getListFromDir() {
+  let files = fs.readdirSync(__dirname)
+  const internalMarkdown = ['README.md']
+
+  return files.filter(fileName => /\d*\.md/gi.test(fileName) && internalMarkdown.indexOf(fileName) === -1)
+  // TODO: sort by create time
+}
+
+async function writeListToMarkdown(list) {
+  let str = '# Tips\n\n'
+
+  list.forEach((fileName, index) => {
+    str += `- [${index+1}-${fileName}](./${fileName})\n`
+  })
+
+  fs.writeFileSync('./README.md', str)
+}
+
+async function main() {
+  const list = await getListFromDir()
+  writeListToMarkdown(list)
+}
+
+main()
